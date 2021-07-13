82a8a0fcb518d13c4aa5fbc36a700550768e77e9
Waseem Sayara <waseem.ahmad1294@gmail.com>
2021-07-13 12:09:06 +0300

    added print results

--- api_test.py
+++ api_test.py
@@ -8,31 +8,37 @@ url = 'https://reqres.in/'
 def test_get_list_users():
     response = requests.get(url+'api/users?page=2')
     assert response.status_code == 200
+    print("get list users : ", response.status_code)
 
 
 def test_get_single_user():
     response = requests.get(url+'api/users?2')
     assert response.status_code == 200
+    print("get single user : ", response.status_code)
 
 
 def test_get_not_existing_user():
     response = requests.get(url+'api/users/23')
     assert response.status_code == 404
+    print("get not existing user : ", response.status_code)
 
 
 def test_get_list_resource():
     response = requests.get(url+'api/unknown')
     assert response.status_code == 200
+    print("get list resource : ", response.status_code)
 
 
 def test_get_single_resource():
     response = requests.get(url+'api/unknown/2')
     assert response.status_code == 200
+    print("get single resource : ", response.status_code)
 
 
 def test_get_not_existing_resource():
     response = requests.get(url+'api/unknown/23')
     assert response.status_code == 404
+    print("get not existing resource : ", response.status_code)
 
 
 def test_post_create_user():
@@ -40,6 +46,7 @@ def test_post_create_user():
     json_data = json.dumps(data)
     response = requests.post(url+'api/users', json_data)
     assert response.status_code == 201
+    print("post create user : ", response.status_code)
 
 
 def test_put_update_user():
@@ -47,6 +54,7 @@ def test_put_update_user():
     json_data = json.dumps(data)
     response = requests.put(url+'api/users/2', json_data)
     assert response.status_code == 200
+    print("put update user : ", response.status_code)
 
 
 def test_patch_update_user():
@@ -54,21 +62,21 @@ def test_patch_update_user():
     json_data = json.dumps(data)
     response = requests.patch(url+'api/users/2', json_data)
     assert response.status_code == 200
+    print("patch update user : ", response.status_code)
 
 
 def test_delete_user():
     response = requests.delete(url+'api/user/2')
     assert response.status_code == 204
+    print("delete user : ", response.status_code)
 
 
 def test_post_register():
     data = {"email": 'eve.holt@reqres.in', "password": "pistol"}
     json_data = json.dumps(data)
     response = requests.post(url+'api/register', data)
-    print(json_data)
-    print(response.content)
-    print(response.request.body)
     assert response.status_code == 200
+    print("post register : ", response.status_code)
 
 
 def test_post_failed_register():
@@ -76,14 +84,15 @@ def test_post_failed_register():
     json_data = json.dumps(data)
     response = requests.post(url+'api/register', json_data)
     assert response.status_code == 400
+    print("post failed register : ", response.status_code)
 
 
 def test_post_login():
     data = {"email": 'eve.holt@reqres.in', "password": "cityslicka"}
     json_data = json.dumps(data)
     response = requests.post(url+'api/login', data)
-    print(response.content)
     assert response.status_code == 200
+    print("post login : ", response.status_code)
 
 
 def test_post_failed_login():
@@ -91,11 +100,13 @@ def test_post_failed_login():
     json_data = json.dumps(data)
     response = requests.post(url+'api/login', json_data)
     assert response.status_code == 400
+    print("post failed login : ", response.status_code)
 
 
 def test_get_list_users_delayed():
     response = requests.get(url+'api/users?delay=3')
     assert response.status_code == 200
+    print("get list users delayed : ", response.status_code)
 
 
 test_get_list_users()
