dbd3b46a037780ffe65c2a9323ce78a8b2e7cc5a
Waseem Sayara <waseem.ahmad1294@gmail.com>
2021-07-15 14:29:15 +0300

    added main

--- main.py
+++ main.py
@@ -4,21 +4,35 @@ import pytest
 args = sys.argv
 
 print(args)
-if args[1] == "all":
-    pytest.main([])
 
-elif args[1] == "get":
-    print("entered get")
-    pytest.main(["test_get_requests.py"])
+if len(args) > 1:
+    if args[1] == "all":
+        pytest.main([])
 
-elif args[1] == "post":
-    pytest.main(["test_post_requests.py"])
+    elif args[1] == "get":
+        print("entered get")
+        pytest.main(["test_get_requests.py"])
 
-elif args[1] == "put":
-    pytest.main(["test_put_requests.py"])
+    elif args[1] == "post":
+        pytest.main(["test_post_requests.py"])
 
-elif args[1] == "patch":
-    pytest.main(["test_patch_requests.py"])
+    elif args[1] == "put":
+        pytest.main(["test_put_requests.py"])
 
-elif args[1] == "delete":
-    pytest.main(["test_delete_requests.py"])
+    elif args[1] == "patch":
+        pytest.main(["test_patch_requests.py"])
+
+    elif args[1] == "delete":
+        pytest.main(["test_delete_requests.py"])
+
+    elif args[1] == "users":
+        pytest.main(["-m users"])
+
+    elif args[1] == "resource":
+        pytest.main(["-m resource"])
+
+    elif args[1] == "register":
+        pytest.main(["-m register"])
+
+    elif args[1] == "login":
+        pytest.main(["-m login"])
